1db362701a95e620faee1adc8d53efcdae90a6c9
Jens Becker <info@jensbecker.dev>
2023-04-22 13:55:32 +0200

    Add imprint and privacy page

---
+++ src/app/impressum/page.tsx
@@ -0,0 +1,52 @@
+export const metadata = {
+  title: "Impressum",
+  robots: {
+    index: false,
+  },
+};
+
+export default function Contact() {
+  return (
+    <>
+      <h1 className="mb-0 text-center">Impressum</h1>
+
+      <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+      <p>
+        Jens Becker
+        <br />
+        Ehranger Str. 212
+        <br />
+        54293 Trier
+      </p>
+
+      <h2>Kontakt</h2>
+      <p>
+        Telefon: +49 1522 8461402
+        <br />
+        E-Mail: info@jensbecker.dev
+      </p>
+
+      <h2>EU-Streitschlichtung</h2>
+      <p>
+        Die Europ&auml;ische Kommission stellt eine Plattform zur
+        Online-Streitbeilegung (OS) bereit:{" "}
+        <a
+          href="https://ec.europa.eu/consumers/odr/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://ec.europa.eu/consumers/odr/
+        </a>
+        .<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.
+      </p>
+
+      <h2>
+        Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+      </h2>
+      <p>
+        Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
+        vor einer Verbraucherschlichtungsstelle teilzunehmen.
+      </p>
+    </>
+  );
+}
