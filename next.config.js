95bf6c7e7186d93f189d39317f1c856b3e31721b
Jens Becker <info@jensbecker.dev>
2023-04-23 13:10:06 +0200

    Fix broken indexed urls by adding redirects

--- next.config.js
+++ next.config.js
@@ -7,6 +7,27 @@ const nextConfig = {
     typedRoutes: true,
     scrollRestoration: true,
   },
+  redirects: async () => {
+    // Redirects to make sure that urls from the old site (now at https://v2.jensbecker.dev)
+    // which are still indexed by Google are redirected to the new site correctly.
+    return [
+      {
+        source: "/services/app-development",
+        destination: "/appentwicklung",
+        permanent: true,
+      },
+      {
+        source: "/services/web-development",
+        destination: "/webentwicklung",
+        permanent: true,
+      },
+      {
+        source: "/projects/:path*",
+        destination: "/referenzen/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
