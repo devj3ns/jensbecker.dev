79b3c93e3c3b06e0f7d5e2c210c7ecdce8401faf
Jens Becker <jnbc1014@hochschule-trier.de>
2024-08-06 14:55:39 +0200

    remove next js site

--- src/app/components/card.tsx
+++
@@ -1,9 +0,0 @@
-import { HoverEffect } from "./hoverEffect";
-
-export function Card({ children }: { children: React.ReactNode }) {
-  return (
-    <HoverEffect className="px-5 py-5 rounded-md bg-neutral-100">
-      {children}
-    </HoverEffect>
-  );
-}
