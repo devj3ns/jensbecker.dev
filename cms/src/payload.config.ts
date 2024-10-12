fa2348016a510a38e83fc4c717c1335149196a68
Jens Becker <info@jhb.software>
2024-10-12 19:41:08 +0200

    clean up payload cms project

--- cms/src/payload.config.ts
+++ cms/src/payload.config.ts
@@ -1,13 +1,12 @@
-// storage-adapter-import-placeholder
 import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { fileURLToPath } from 'url'
 
-import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { Users } from './collections/Users'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
