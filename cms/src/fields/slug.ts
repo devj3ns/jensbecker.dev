db374e9048efad70e96d4282126868c7935aff0c
Jens Becker <info@jhb.software>
2024-10-19 14:25:50 +0200

    make slug field unique

--- cms/src/fields/slug.ts
+++ cms/src/fields/slug.ts
@@ -12,6 +12,7 @@ export const slugField: Slug = (fieldToUse = 'title', overrides = {}) =>
       label: 'Slug',
       type: 'text',
       index: true,
+      unique: true,
       required: true,
       localized: true,
       admin: {
