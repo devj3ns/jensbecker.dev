a23d06118c2a16d5e5a2f3bae4e46d9d581de020
Jens Becker <info@jhb.software>
2024-11-27 20:44:00 +0100

    use headless lexical editor to convert to plain text

--- cms/src/plugins/payload-ai-meta-description/utils/lexicalToPlainText.ts
+++ cms/src/plugins/payload-ai-meta-description/utils/lexicalToPlainText.ts
@@ -1,61 +1,48 @@
 import payloadConfig from '@/payload.config'
-import {
-  HTMLConverterFeature,
-  consolidateHTMLConverters,
-  convertLexicalToHTML,
-  defaultEditorFeatures,
-  sanitizeServerEditorConfig,
-} from '@payloadcms/richtext-lexical'
+import { getEnabledNodes } from '@payloadcms/richtext-lexical'
 
-import { defaultEditorConfig } from '@payloadcms/richtext-lexical'
-import { convert } from 'html-to-text'
+import { defaultEditorConfig, sanitizeServerEditorConfig } from '@payloadcms/richtext-lexical'
+import { $getRoot } from '@payloadcms/richtext-lexical/lexical'
+import { createHeadlessEditor } from '@payloadcms/richtext-lexical/lexical/headless'
 
 /** Converts the data from a lexical field to plain text */
 export async function lexicalToPlainText(data: any): Promise<string | undefined> {
-  // TODO: try to use the lexical headless editor to convert to plaintext directly
-  // see https://payloadcms.com/docs/rich-text/lexical#lexical-plain-text
-  //
-  // What I tried so far:
-  //
-  // const headlessEditor = createHeadlessEditor({})
-
-  // headlessEditor.setEditorState(headlessEditor.parseEditorState(excerptRaw))
-
-  // const plainTextContent = headlessEditor.getEditorState().read(() => {
-  //   return $getRoot().getTextContent()
-  // })
-
-  const editorConfig = {
-    ...defaultEditorConfig,
-    features: [...defaultEditorFeatures, HTMLConverterFeature({})],
+  if (!data) {
+    console.warn('No data to convert to plain text. Returning undefined.')
+    return undefined
   }
 
-  const sanitizedServerEditorConfig = await sanitizeServerEditorConfig(
-    editorConfig,
-    await payloadConfig,
-  )
+  // Docs: https://payloadcms.com/docs/lexical/converters#headless-editor
 
-  const html = await convertLexicalToHTML({
-    converters: consolidateHTMLConverters({ editorConfig: sanitizedServerEditorConfig }),
-    data: data,
-  })
+  try {
+    const headlessEditor = createHeadlessEditor({
+      nodes: getEnabledNodes({
+        editorConfig: await sanitizeServerEditorConfig(defaultEditorConfig, await payloadConfig),
+      }),
+    })
 
-  const text = await htmlToPlainText(html)
+    const parsedEditorState = headlessEditor.parseEditorState(data)
 
-  // Returning undefined if the text is empty, so that the ?? operator can be used for the output
-  return text ? text : undefined
-}
+    headlessEditor.update(
+      () => headlessEditor.setEditorState(parsedEditorState),
+      { discrete: true }, // This should commit the editor state immediately
+    )
 
-async function htmlToPlainText(html: string) {
-  return convert(html, {
-    selectors: [
-      {
-        selector: 'a',
-        options: {
-          ignoreHref: true,
-        },
-      },
-    ],
-    wordwrap: null,
-  })
+    let plainTextContent = headlessEditor.getEditorState().read(() => $getRoot().getTextContent())
+
+    // Remove linebreaks and multiple spaces
+    plainTextContent = plainTextContent
+      .replace(/(\r\n|\n|\r)/gm, ' ')
+      .replace(/\s+/g, ' ')
+      .trim()
+
+    // Returning undefined if the text is empty, so that we can use the ?? operator in the excerpt transformer
+    return plainTextContent ? plainTextContent : undefined
+  } catch (e) {
+    console.error({
+      message: 'ERROR parsing lexical to plaintext.',
+      error: e,
+    })
+    return undefined
+  }
 }
