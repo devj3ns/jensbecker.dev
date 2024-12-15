cc4124a3a8eea094404e1dabe953554fc31d7f64
Jens Becker <info@jhb.software>
2024-12-15 13:23:40 +0100

    feat(cms): add virtual testimonials field to testimonials block

--- cms/src/blocks/TestimonialsBlock.ts
+++ cms/src/blocks/TestimonialsBlock.ts
@@ -14,7 +14,38 @@ export const TestimonialsBlock: Block = {
     },
   },
   fields: [
-    // unfortunately join fields are not supported inside blocks
-    // -> the testimonials have to be fetched manually in the frontend
+    {
+      // This virtual field holds all published testimonials, so that they are directly available to the frontend
+      name: 'testimonials',
+      type: 'relationship',
+      relationTo: 'testimonials',
+      hasMany: true,
+      required: true,
+      virtual: true,
+      admin: {
+        readOnly: true,
+      },
+      hooks: {
+        afterRead: [
+          async ({ req: { payload } }) => {
+            const testimonials = await payload.find({
+              collection: 'testimonials',
+              limit: 100,
+              select: {
+                // only select the id
+              },
+              where: {
+                _status: {
+                  equals: 'published',
+                },
+              },
+              draft: false,
+            })
+
+            return testimonials.docs.map((testimonial) => testimonial.id)
+          },
+        ],
+      },
+    },
   ],
 }
